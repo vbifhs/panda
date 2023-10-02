87cf9c061273df80fd2222cc9481e7b532425dc0
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-10-02 15:48:59 -0700

    update cppcheck to 2.12.1

--- tests/misra/install.sh
+++ tests/misra/install.sh
@@ -9,8 +9,8 @@ if [ ! -d "$CPPCHECK_DIR" ]; then
 fi
 
 cd $CPPCHECK_DIR
-git fetch
-git checkout e1cff1d1ef92f6a1c6962e0e4153b7353ccad04c
+git fetch --all
+git checkout 65da79394471784641568dc4ac5da5466599809d
 git -c user.name=a -c user.email="a@b.c" cherry-pick 7199dde1618b5166735f07619dcdb9f4eafdb557
 make clean
 make MATCHCOMPILTER=yes CXXFLAGS="-O2" -j8
