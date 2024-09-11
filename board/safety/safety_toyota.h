1158c705a1042dcabb395ea34124c72a92363fd8
vbifhs <100491926+vbifhs@users.noreply.github.com>
2024-09-11 12:54:55 -0700

    added GET_ADDR function

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -154,7 +154,8 @@ static int toyota_rx_hook(CANPacket_t *to_push) {
   //   }
   //   generic_rx_checks((addr == 0x180));
   // }
-  if(valid && (addr == (toyota_driving_bus ? 0x280 : 0x689)) )  {
+  if(valid && (GET_ADDR(to_push) == (toyota_driving_bus ? 0x280 : 0x689)) )  {
+    int addr = GET_ADDR(to_push);
     if(addr == 0x689){
       bool cruise_engaged = GET_BIT(to_push, 17U) != 0U;
       pcm_cruise_check(cruise_engaged);}
