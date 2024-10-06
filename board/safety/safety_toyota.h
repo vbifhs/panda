b23ccc7cf1ae0f29ff1e0ce1be17366842e31272
vbifhs <100491926+vbifhs@users.noreply.github.com>
2024-10-05 20:23:16 -0700

    added  msg 0x689 to DRX ADDR CHK

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -54,6 +54,7 @@ addr_checks toyota_steering_bus_rx_checks = SET_ADDR_CHECKS(toyota_steering_bus_
 AddrCheckStruct toyota_driving_bus_addr_checks[] = {
   {.msg = {{ 0xB0, 0, 8, .check_checksum = false, .expected_timestep = 12000U}, { 0 }, { 0 }}},
   {.msg = {{ 0xB2, 0, 8, .check_checksum = false, .expected_timestep = 12000U}, { 0 }, { 0 }}},
+  {.msg = {{0x689, 1, 8, .check_checksum = false, .expected_timestep = 1000000U}, { 0 }, { 0 }}},
   {.msg = {{0x2C1, 0, 8, .check_checksum = false, .expected_timestep = 32000U}, { 0 }, { 0 }}},
 };
 addr_checks toyota_driving_bus_rx_checks = SET_ADDR_CHECKS(toyota_driving_bus_addr_checks);
