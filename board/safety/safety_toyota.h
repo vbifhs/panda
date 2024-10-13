8a2b121a02c728981f78b70e6f76afcd4c37a000
vbifhs <100491926+vbifhs@users.noreply.github.com>
2024-10-12 20:40:20 -0700

    set valid to true

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -93,8 +93,8 @@ static uint32_t toyota_get_checksum(CANPacket_t *to_push) {
 
 static int toyota_rx_hook(CANPacket_t *to_push) {
 
-  bool valid = addr_safety_check(to_push, toyota_driving_bus ? (&toyota_driving_bus_rx_checks ) : (&toyota_steering_bus_rx_checks ),
-                                 toyota_get_checksum, toyota_compute_checksum, NULL, NULL);
+  bool valid = true;//addr_safety_check(to_push, toyota_driving_bus ? (&toyota_driving_bus_rx_checks ) : (&toyota_steering_bus_rx_checks ),
+                                 //toyota_get_checksum, toyota_compute_checksum, NULL, NULL);
 
 
   //if (valid && (GET_BUS(to_push) == 0U)) {
